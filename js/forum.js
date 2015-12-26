be8c755ee9c8b6c34e96694b157719f8e5cf82ea
Mathias Bynens <mathias@qiwi.be>
2015-12-26 12:52:33 +0100

    Remember the forum posting character

--- js/forum.js
+++ js/forum.js
@@ -3,6 +3,7 @@
 // Improve forum usability.
 if (/forum(?:\.test)?\.tibia\.com$/.test(location.hostname)) {
 
+	// Make individual posts easily permalinkable.
 	const regexThreadID = /^Thread\x20#/;
 	let threadID = '';
 	each(document.querySelectorAll('b'), function(el) {
@@ -20,5 +21,30 @@ if (/forum(?:\.test)?\.tibia\.com$/.test(location.hostname)) {
 		el.textContent = '\xB6';
 		el.className = 'mths-tibia-permalink';
 	});
+
+	// Remember the character used for posting.
+	const select = document.querySelector('select[name="forum_character"]');
+	if (select) {
+		const separator = '|';
+		const storedCharacter = localStorage.forumCharacter;
+		if (storedCharacter) {
+			const parts = storedCharacter.split(separator);
+			const value = parts[0];
+			const character = parts[1];
+			each(select.options, function(option, index) {
+				if (option.value == value && option.textContent == character) {
+					select.selectedIndex = index;
+					return false; // break
+				}
+			});
+		}
+		select.oninput = function() {
+			const index = select.selectedIndex;
+			const value = select.value;
+			const character = select.options[index].textContent;
+			const id = `${ value }${ separator }${ character }`;
+			localStorage.forumCharacter = id;
+		};
+	}
 
 }
